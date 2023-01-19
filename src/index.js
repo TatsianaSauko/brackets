8f3458069e762103711d31f658affe783b234168
TatsianaSauko <miljanzowa@mail.ru>
2023-01-19 15:11:49 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function removePairsOfBrackets(a, b) {
+    for (let item of b) {
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] === item[0] && a[i + 1] === item[1]) {
+                a.splice(i, 2)
+            }
+        }
+    }
+    return a
+}
+
+function checksCycleCondition(n, m) {
+    let clone = Object.assign([], n);
+    let lstN = removePairsOfBrackets(n, m)
+    return lstN.length < clone.length
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let lst = str.split('')
+    let lstNew = []
+    while (checksCycleCondition(lst, bracketsConfig)) {
+        lstNew = removePairsOfBrackets(lst, bracketsConfig)
+        lst = lstNew.splice(0)
+    }
+    return lst.length === 0
 }
